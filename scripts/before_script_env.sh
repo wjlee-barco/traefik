c259af385173956e9e6bdf7485f0f73538b1b248
WJLEE <wj.lee@barco.com>
2020-01-19 19:45:21 +0800

    enable gitlab-ci

---
+++ scripts/before_script_env.sh
@@ -0,0 +1,10 @@
+#
+# 1.1 Check if .env file exists
+#
+if [ -e ../.env ]; then
+    source ../.env
+else 
+    echo "It seems you didn´t create your .env file, so we will create one for you."
+    cp ../.env.sample ../.env
+    # exit 1
+fi
---
+++ scripts/before_script_install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#
+# Step 1: This file first update env with your .env file
+
+#
+# Step 2: Update necessary components and docker
+# 
+# 2.1 lsb_release
+# 2.2 curl
+# 2.3 ntp
+# 2.4 docker
+# 2.5 docker-compose
+#
+sudo apt-get -y install lsb-core
+sudo apt-get -y install curl
+# install ntp
+sudo apt-get -y install ntp
+sudo service ntp restart
+    
+# docker installation
+sudo curl -sSL https://get.docker.com | sh
+sudo usermod -aG docker root
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+exit 0
